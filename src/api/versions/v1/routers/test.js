949863d9f666650f80fa14002110fa17eefa0160
PizzaBoss <68411735+PizzaBossXD@users.noreply.github.com>
2022-03-31 20:20:01 +0300

    Fixed my word mistake! 🤦

--- src/api/versions/v1/routers/test.js
+++ src/api/versions/v1/routers/test.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.status(200).send({ message: `Thanks for standing by test version 1` }); // Change or remove this if you want to.
+    res.status(200).send({ message: `Thanks for stopping by test version 1` }); // Change or remove this if you want to.
 });
 
 module.exports = router;
